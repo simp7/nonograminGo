e81a3bbf05c2c3ab9e00bbfe1e8be30a5444a743
N0RM4L15T <JHParkOfficial@protonmail.com>
2019-03-14 17:36:55 +0900

    Replace old control.go

--- control/control.go
+++ control/control.go
@@ -1,64 +1,125 @@
 package control
 
-/*
 import (
+	"../asset"
+	"../model"
+	"../util"
 	"bufio"
+	"github.com/nsf/termbox-go"
+	"io/ioutil"
 	"os"
 )
 
-// This file deals with user's control.
-
-type keyStroker struct {
-	reader *bufio.Reader
-	key    byte
-	err    error
+type KeyReader struct {
+	eventChan chan termbox.Event
+	endChan   chan struct{}
 }
 
-func NewKeyStroker() *keyStroker {
-	rd := keyStroker{}
-	rd.reader = bufio.NewReader(os.Stdin)
-	rd.key = 0
-	rd.err = nil
+func NewKeyReader() *KeyReader {
+
+	rd := KeyReader{}
+	rd.eventChan = make(chan termbox.Event)
+	rd.endChan = make(chan struct{})
 	return &rd
+
 }
 
-func (rd *keyStroker) ControlMenu() {
-	for {
-		rd.key, rd.err = rd.reader.ReadByte()
-		CheckErr(rd.err)
-		switch rd.key {
-		case '1':
-			ShowMapList()
-			rd.SelectMap()
-		case '2':
-
-		case '3':
-			ShowResult()
-			rd.PressAnyKey()
-		case '4':
-			return
-		}
-	}
-}
-
-func (rd *keyStroker) SelectMap() {
-	var mapName []byte
-	for {
-		mapName, _, rd.err = rd.reader.ReadLine()
-		CheckErr(rd.err)
-		targetMap := newNonomap(string(mapName))
-	}
-}
-
-func (rd *keyStroker) ControlGame() {
-	for {
-		rd.key, rd.err = rd.reader.ReadByte()
-		CheckErr(rd.err)
-
-	}
-}
-
-func (rd *keyStroker) PressAnyKey() { //calls when entering hall of honor
-	rd.reader.ReadByte()
-}
+/*
+This function takes user input into channel.
+This function will be called when program starts.
 */
+
+func (rd *KeyReader) Control() {
+
+	err := termbox.Init()
+	util.CheckErr(err)
+	defer termbox.Close()
+
+	go func() {
+		for {
+			rd.eventChan <- termbox.PollEvent()
+		}
+	}()
+
+	rd.menu()
+
+	<-rd.endChan
+	close(rd.eventChan)
+}
+
+func (rd *KeyReader) refresh() {
+	termbox.Clear(asset.ColorEmptyCell, asset.ColorEmptyCell)
+}
+
+func (rd *KeyReader) printf(x int, y int, msgs []string) {
+
+	temp := x
+
+	for _, msg := range msgs {
+		for _, ch := range msg {
+			termbox.SetCell(x, y, ch, asset.ColorText, asset.ColorEmptyCell)
+			x++
+		}
+		x = temp
+		y++
+	}
+
+}
+
+func (rd *KeyReader) menu() {
+
+	for {
+
+		rd.refresh()
+		rd.printf(5, 5, asset.StringMainMenu)
+		termbox.Flush()
+		event := <-rd.eventChan
+
+		if event.Type == termbox.EventKey {
+			switch {
+			case event.Ch == '1':
+				rd.selectMap()
+			case event.Ch == '2':
+			case event.Ch == '3':
+			case event.Ch == '4' || event.Key == termbox.KeyEsc:
+				close(rd.endChan)
+				return
+			}
+
+		}
+
+	}
+
+}
+
+func (rd *KeyReader) selectMap() {
+
+	nameReader := bufio.NewReader(os.Stdin)
+
+	for {
+		rd.selectMap()
+		termbox.SetCursor(3, 13)
+		mapName, _, err := nameReader.ReadLine()
+		util.CheckErr(err)
+		targetMap := model.NewNonomap(string(mapName))
+		targetMap.CompareMap(*targetMap, 3)
+	}
+
+}
+
+func (rd *KeyReader) controlGame() {
+	for {
+	}
+}
+
+func (rd *KeyReader) showMapList() {
+	files, err := ioutil.ReadDir("../maps")
+	util.CheckErr(err)
+	mapList := make([]string, 10)
+
+	for _, file := range files {
+		mapList = append(mapList, file.Name())
+	}
+
+	rd.printf(3, 3, mapList)
+}
