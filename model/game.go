f9bc8afbd7b07662c0a136dea2ccd5e302485e9d
N0RM4L15T <JHParkOfficial@protonmail.com>
2019-06-04 20:38:33 +0900

    Add bitmap in model to easily perform functions.

--- model/game.go
+++ model/game.go
@@ -8,14 +8,15 @@ import (
 )
 
 /*
-This file deals with algoritms of whole game of nonogram.
-User's control or display should be seperated from this file.
+	This file deals with algoritms of whole game of nonogram.
+	User's control or display should be seperated from this file.
 */
 
 type Nonomap struct {
 	width   int
 	height  int
 	mapdata []int
+	bitmap  [][]bool
 }
 
 /*
@@ -25,8 +26,11 @@ type Nonomap struct {
 
 	Rest elements indicates actual map which player has to solve.
 	Each elements indicates map data of each line.
-	is designated by bitmap, which 0 is blank and 1 is filled one.
-	Because the size of int is 32bits, width of maps can't be more than 32 bits.
+	They are designated by bitmap, which 0 is blank and 1 is filled one.
+
+	Because the size of int is 32bits, width of maps can't be more than 32 mathmatically.
+
+	But because of display's limit, width and height can't be more than 25
 
 	When it comes to player's map, 2 is checked one where player thinks that cell is blank.
 
@@ -54,7 +58,7 @@ func NewNonomap(data string) *Nonomap {
 	}
 	//Extract map's answer from file.
 
-	if imported.height > 30 || imported.width > 30 {
+	if imported.height > 25 || imported.width > 25 || imported.height < 0 || imported.width < 0 {
 		util.CheckErr(util.InvalidMap)
 	} //Check if height and width meets criteria of size.
 
@@ -68,14 +72,14 @@ func NewNonomap(data string) *Nonomap {
 	} //Check whether height matches mapdata.
 
 	//Check validity of file.
-
+	imported.bitmap = convertToBitmap(imported.width, imported.height, imported.mapdata)
 	return &imported
 
 }
 
 /*
-This function compares selected row's player data and answer data so it can judge if player painted wrong cell.
-This function will be called when player paints cell(NOT when checking).
+	This function compares selected row's player data and answer data so it can judge if player painted wrong cell.
+	This function will be called when player paints cell(NOT when checking).
 */
 
 func (nm *Nonomap) CompareMap(answer Nonomap, x int) bool {
@@ -95,4 +99,27 @@ func (nm *Nonomap) EmptyMap() *Nonomap {
 		empty.mapdata[n] = 0
 	}
 	return empty
+}
+
+/*
+func (nm *Nonomap) CreateProblem() (horizontal [][]int, vertical [][]int) {
+
+}
+*/
+func convertToBitmap(width int, height int, mapdata []int) [][]bool {
+
+	bitmap := make([][]bool, height)
+	for n := range bitmap {
+		bitmap[n] = make([]bool, width)
+	}
+
+	for n, v := range mapdata {
+		for i := 1; i <= width; i++ {
+			bitmap[width-i][n] = (v%2 == 1)
+			v = v / 2
+		}
+	}
+
+	return bitmap
+
 }
