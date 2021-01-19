f8babb62625acc5682c1d630939919e263409484
simp7 <JHParkOfficial@protonmail.com>
2021-01-19 09:59:47 +0900

    introduce window constant and drawer

--- asset/figure.go
+++ asset/figure.go
@@ -1,9 +1,12 @@
 package asset
 
+import "github.com/simp7/nonograminGo/util"
+
 type Figure struct {
-	NameMax   int
-	WidthMax  int
-	HeightMax int
-	DefaultX  int
-	DefaultY  int
+	NameMax    int
+	WidthMax   int
+	HeightMax  int
+	DefaultX   int
+	DefaultY   int
+	DefaultPos util.Pos
 }
--- nonogram.go
+++ nonogram.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"github.com/simp7/nonograminGo/control"
+	"github.com/simp7/nonograminGo/util"
+	"os"
 )
 
-func main() {
+var rd control.Controller = control.NewCliController()
 
-	var rd control.Controller
-	rd = control.NewCliController()
+func init() {
+
+	if len(os.Args) > 2 {
+		util.CheckErr(util.TooManyArgs)
+	} else if os.Args[1] == "alpha" {
+		rd = control.NewImprovedController()
+	}
+
+}
+
+func main() {
 
 	rd.Start()
 
