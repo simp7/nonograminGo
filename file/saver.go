7e0a3f38cfe2d3d0ce6bc6c55cfca64c8f11b7e9
simp7 <JHParkOfficial@protonmail.com>
2021-05-07 09:04:32 +0900

    Add comments of function in interface of file package.

--- file/saver.go
+++ file/saver.go
@@ -1,6 +1,7 @@
 package file
 
 //Saver is an interface that saves data from the program to somewhere.
+//Save saves data from argument to Saver.
 type Saver interface {
 	Save(interface{}) error
 }
