42cbc60f78e94fa79bf19668684adb80711376d5
simp7 <JHParkOfficial@protonmail.com>
2021-05-16 18:15:40 +0900

    Add setting packages for general settings of clients.

---
+++ setting/text.go
@@ -0,0 +1,36 @@
+package setting
+
+//Text is an data of text that are not yet be processed but stored.
+type Text struct {
+	FileVersion     string
+	Title           string
+	SelectRequest   string
+	Start           string
+	Create          string
+	Help            string
+	Credit          string
+	Exit            string
+	MapList         string
+	Prev            string
+	Next            string
+	Result          string
+	Clear           string
+	MapName         string
+	ClearTime       string
+	WrongCells      string
+	CompleteMsg     string
+	ExplArrowKey    string
+	ExplSpace       string
+	ExplX           string
+	ExplEnter       string
+	ExplEsc         string
+	DeveloperInfo   string
+	License         string
+	ThankYouMsg     string
+	ReqMapName      string
+	ReqWidth        string
+	ReqHeight       string
+	MapSizeError    string
+	MapFileNotExist string
+	ArrowKey        string
+}
